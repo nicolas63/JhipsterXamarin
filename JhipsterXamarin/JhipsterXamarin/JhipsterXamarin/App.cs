5489f7abe142c3cee595325c14215b72c928c30d
Zenrac <zenrac@outlook.fr>
2020-10-22 09:45:12 +0200

    Base app

---
+++ JhipsterXamarin/JhipsterXamarin/JhipsterXamarin/App.cs
@@ -0,0 +1,21 @@
+﻿using MvvmCross;
+using MvvmCross.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using JhipsterXamarin.Services;
+using JhipsterXamarin.ViewModels;
+using JhipsterXamarin.Views;
+
+namespace JhipsterXamarin
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            Mvx.IoCProvider.RegisterType<IListService, ListService>();
+
+            RegisterAppStart<ListViewModel>();
+        }
+    }
+}
