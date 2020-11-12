28d0cb16fd5674b47eaf64568b6c393f490268eb
Zenrac <zenrac@outlook.fr>
2020-11-12 14:43:45 +0100

    Update doc

--- JhipsterXamarin/JhipsterXamarin/JhipsterXamarin/LinkerPreserve.cs
+++ JhipsterXamarin/JhipsterXamarin/JhipsterXamarin/LinkerPreserve.cs
@@ -4,7 +4,8 @@ using MvvmCross;
 namespace JhipsterXamarin
 {
     /// <summary>
-    /// Linker preserve for Akavache (https://github.com/reactiveui/Akavache).
+    /// Linker preserve for Akavache (https://github.com/reactiveui/Akavache#handling-xamarin-linker).
+    /// Ensures the Akavache.Sqlite3 dll will not be removed by Xamarin build tools.
     /// Note: This class file is *required* for iOS to work correctly.
     /// </summary>
     [Preserve]
