e2a4fd2648703823241754af5974a886da7cfdef
Thomas-Julien-SAHUT <73140592+Thomas-Julien-SAHUT@users.noreply.github.com>
2020-11-12 16:40:47 +0100

    Added UT login

--- JhipsterXamarin/UnitTestJhispterXamarin/TestLogin.cs
+++ JhipsterXamarin/UnitTestJhispterXamarin/TestLogin.cs
@@ -1,12 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using JhipsterXamarin.Models;
+using JhipsterXamarin.Services;
+using JhipsterXamarin.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using MvvmCross.Navigation;
+using MvvmCross.Tests;
 
 namespace UnitTestJhispterXamarin
 {
-    public class TestLogin
+    [TestClass]
+    public class TestLogin : MvxIoCSupportingTest
     {
+        LoginViewModel loginViewModel;
+        LoginModel loginModel = new LoginModel();
+
+        [TestMethod]
+        public void TestViewModel()
+        {
+            initializeAll();
+        }
+        public void initializeAll()
+        {
+            base.Setup(); // from MvxIoCSupportingTest
+            AdditionalSetup();
+            loginViewModel.Initialize();
+        }
+        protected override void AdditionalSetup()
+        {
+            var mockAuthService = new Mock<IAuthenticationService>();
+            var mockNavLoginService = new Mock<IMvxNavigationService>();
+            loginViewModel = new LoginViewModel(mockNavLoginService.Object, mockAuthService.Object);
+
+            Ioc.RegisterSingleton<IAuthenticationService>(mockAuthService.Object);
+            Ioc.RegisterSingleton<IMvxNavigationService>(mockNavLoginService.Object);
+        }
+        [TestMethod]
+        public void TestSignInAdmin()
+        {
+            initializeAll();
+
+            loginModel.Username = "admin";
+            loginModel.Password = "admin";
+            loginModel.RememberMe = false;
+
+            loginViewModel.Username = "admin";
+            loginViewModel.Password = "admin";
+            loginViewModel.RememberMe = false;
+
+            Assert.AreEqual(loginViewModel.GetAuthenticationService().SignIn(loginModel).ToString(), loginViewModel.SignInConnection().ToString(), "Test failed : login error user : admin  | password : admin");
+        }
+        [TestMethod]
+        public void TestSignOutAdmin()
+        {
+            initializeAll();
+            loginModel.Username = "admin";
+            loginModel.Password = "admin";
+            loginModel.RememberMe = false;
+            loginViewModel.GetAuthenticationService().SignIn(loginModel);
+            var result = loginViewModel.GetAuthenticationService().SignOut();
+            Assert.AreEqual(null,result , "Test failed : logout error");
+        }
     }
 }
