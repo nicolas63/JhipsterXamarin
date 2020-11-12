bb931f58669280e27b4084ecc78847b7a46e8351
Zenrac <zenrac@outlook.fr>
2020-11-12 14:11:10 +0100

    Documentation update

--- JhipsterXamarin/JhipsterXamarin/JhipsterXamarin/LinkerPreserve.cs
+++ JhipsterXamarin/JhipsterXamarin/JhipsterXamarin/LinkerPreserve.cs
@@ -1,10 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Akavache.Sqlite3;
+﻿using Akavache.Sqlite3;
+using MvvmCross;
 
 namespace JhipsterXamarin
 {
+    /// <summary>
+    /// Linker preserve for Akavache (https://github.com/reactiveui/Akavache).
+    /// Note: This class file is *required* for iOS to work correctly.
+    /// </summary>
+    [Preserve]
     public static class LinkerPreserve
     {
         static LinkerPreserve()
