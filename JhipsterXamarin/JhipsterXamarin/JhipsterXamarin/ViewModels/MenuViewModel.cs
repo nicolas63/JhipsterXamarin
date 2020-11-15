600b461ed6a7cbe155aed6e4786e4a87b4f7e901
Thomas-Julien-SAHUT <73140592+Thomas-Julien-SAHUT@users.noreply.github.com>
2020-11-15 16:42:13 +0100

    Updated UT

---
+++ JhipsterXamarin/JhipsterXamarin/JhipsterXamarin/ViewModels/MenuViewModel.cs
@@ -0,0 +1,20 @@
+﻿using MvvmCross.Commands;
+using MvvmCross.Navigation;
+
+namespace JhipsterXamarin.ViewModels
+{
+    public class MenuViewModel : BaseViewModel
+    {
+        private readonly IMvxNavigationService _navigationService;
+        public IMvxCommand ShowMyEntitiesCommand { get; }
+        public IMvxCommand ShowLoginCommand { get; }
+
+        public MenuViewModel(IMvxNavigationService navigationService)
+        {
+            _navigationService = navigationService;
+
+            ShowMyEntitiesCommand = new MvxAsyncCommand(async () => await _navigationService.Navigate<MyEntityViewModel>());
+            ShowLoginCommand = new MvxAsyncCommand(async () => await _navigationService.Navigate<LoginViewModel>());
+        }
+    }
+}
---
+++ JhipsterXamarin/JhipsterXamarin/JhipsterXamarin/Views/HomeView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using JhipsterXamarin.ViewModels;
+using MvvmCross.Forms.Presenters.Attributes;
+using MvvmCross.Forms.Views;
+using Xamarin.Forms;
+
+namespace JhipsterXamarin.Views
+{
+    [MvxMasterDetailPagePresentation(MasterDetailPosition.Root, WrapInNavigationPage = false, NoHistory = true)]
+    public partial class HomeView : MvxMasterDetailPage<HomeViewModel>
+    {
+        private bool _firstTime = true;
+
+        public HomeView()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            if (_firstTime)
+            {
+                ViewModel.ShowMenuViewModelCommand.Execute(null);
+                ViewModel.ShowEntitiesViewModelCommand.Execute(null);
+
+                _firstTime = false;
+            }
+
+            base.OnAppearing();
+        }
+    }
+}
