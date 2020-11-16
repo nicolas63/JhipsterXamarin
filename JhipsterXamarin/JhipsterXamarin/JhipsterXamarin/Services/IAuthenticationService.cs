65c20f54cef0460fa94eba5bd79c06d520c1f689
Thomas-Julien-SAHUT <73140592+Thomas-Julien-SAHUT@users.noreply.github.com>
2020-11-16 17:58:18 +0100

    Updated IAuthenticationService

--- JhipsterXamarin/JhipsterXamarin/JhipsterXamarin/Services/IAuthenticationService.cs
+++ JhipsterXamarin/JhipsterXamarin/JhipsterXamarin/Services/IAuthenticationService.cs
@@ -1,5 +1,4 @@
 ﻿using System.Net.Http;
-using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using JhipsterXamarin.Models;
 
@@ -9,8 +8,7 @@ namespace JhipsterXamarin.Services
     {
         bool IsAuthenticated { get; set; }
         UserModel CurrentUser { get; set; }
-        HttpClient _httpClient { get; set; }
         Task<bool> SignIn(LoginModel model);
-        string SignOut();
+        void SignOut();
     }
 }
